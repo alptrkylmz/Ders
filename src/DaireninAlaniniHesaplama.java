81ffb2f7e4bfb16ff64ded0e2a56e8ce0a945204
Alp <bediralpt@gmail.com>
2023-04-25 01:05:42 +0300

    Bug fix : alan formülü düzeltildi , sınıf ismi ve kelime hataları düzetildi.

---
+++ src/DaireninAlaniniHesaplama.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DaireninAlaniniHesaplama {
+
+	public static void main(String[] args) {
+		
+		try (Scanner tara = new Scanner(System.in)) {
+			
+			double daireninAlani ;
+			double pi =  3.14 ;
+			int r ;
+			
+				System.out.println("Lütfen dairenin yarı çapını giriniz...");
+				r = tara.nextInt() ;
+				
+					daireninAlani = pi*(r*r) ;
+						System.out.println("Dağirenizin alanı : " + daireninAlani);
+		}
+	}	
+	}
+
