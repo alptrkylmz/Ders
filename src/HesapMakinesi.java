909bc66ba60858f92b9638d9413fa2949dad0179
Alp <bediralpt@gmail.com>
2023-05-07 00:30:32 +0300

    Hesap makinesi yapıldı

--- src/HesapMakinesi.java
+++ src/HesapMakinesi.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class HesapMakinesi {
 
 	public static void main(String[] args) {
-		int toplam ;
+		double sonuc ;
 		int secenek ;
-		int istenilen1 ;
-		int istenilen2 ;
+		Double istenilen1 ;
+		Double istenilen2 ;
 		    try (Scanner tara = new Scanner(System.in)) {
 				System.out.println("Lüfen işlem yapmak istediğiniz ilk sayıyı giriniz.");
-					istenilen1 = tara.nextInt() ;
+					istenilen1 = tara.nextDouble() ;
 				System.out.println("Lütfen işlem yapmak istediğiniz ikinci sayıyı giriniz.");
-					istenilen2 = tara.nextInt() ;
+					istenilen2 = tara.nextDouble() ;
 					
 						System.out.println("Yapmak istediğiniz aşağıdan seçiniz:");
 						System.out.println("1 Toplama") ;
@@ -19,36 +19,40 @@ public class HesapMakinesi {
 						System.out.println("3 Çarpma") ;
 						System.out.println("4 Bölme");
 							secenek = tara.nextInt() ;
-							while (secenek >=5) {
-								System.out.println(secenek + " 4'den büyük.Lütfen tekrar deneyiniz.");
-								secenek = tara.nextInt() ;
+//							while (secenek >=5) {
+//								System.out.println(secenek + " 4'den büyük.Lütfen tekrar deneyiniz.");
+//								secenek = tara.nextInt() ;
+//							}
+							
+							switch (secenek) {
+							case 1: {
+								sonuc = istenilen1 + istenilen2 ;
+								System.out.println("Toplamınız " + sonuc);
+								break ;
 							}
+							case 2: {
+								sonuc = istenilen1 - istenilen2 ;
+								System.out.println("Farkınız " + sonuc);
+								break ;
+							}
+							
+							case 3:{
+								sonuc = istenilen1 * istenilen2 ;
+								System.out.println("Çarpımınız " + sonuc);
+								break ;
+							}		
+							case 4:{
+								sonuc = istenilen1 / istenilen2 ;
+								System.out.println("Bölümünüz " + sonuc);
+							}
+							default:{
+								System.out.println("Lütfen 1,2,3,4 seçeneklerinden birini seçiniz.");
+								secenek = tara.nextInt() ;
+									
+							}
+							
+							}
+		    }
 			}
-		    
-				switch (secenek) {
-				case 1: {
-					toplam = istenilen1 + istenilen2 ;
-					System.out.println("Toplamınız " + toplam);
-					break ;
-			}
-				case 2: {
-					toplam = istenilen1 - istenilen2 ;
-					System.out.println("Farkınız " + toplam);
-					break ;
-				}
-				
-				case 3:{
-					toplam = istenilen1 * istenilen2 ;
-					System.out.println("Çarpımınız " + toplam);
-					break ;
-				}		
-				case 4:{
-					toplam = istenilen1 / istenilen2 ;
-					System.out.println("Bölümünüz " + toplam);
-			}
-			
-			
-			}
-	}
 
 }
